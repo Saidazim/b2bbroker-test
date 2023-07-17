eb66ac308b0379ec36140572ccb63f0c76b884aa
saidazimkhuja <saidazimkhuja.salikhodjaev@ketteq.com>
2023-07-17 13:07:15 +0300

    feature/design template and use worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'b2bbroker-test';
-}
+  timerValue: number = 1000;
+  arraySize: number = 10;
+  additionalIds: string = '';
+  data: any[] = [];
+  private worker!: Worker;
 
-if (typeof Worker !== 'undefined') {
-  // Create a new
-  const worker = new Worker(new URL('./app.worker', import.meta.url));
-  worker.onmessage = ({ data }) => {
-    console.log(`page got message: ${data}`);
-  };
-  worker.postMessage('hello');
-} else {
-  // Web Workers are not supported in this environment.
-  // You should add a fallback so that your program still executes correctly.
+  ngOnInit() {
+    if (typeof Worker !== 'undefined') {
+      this.worker = new Worker(new URL('./app.worker', import.meta.url));
+      this.worker.onmessage = ({ data }) => {
+        console.log('page got message:', data);
+        this.data = data;
+      };
+      this.worker.postMessage({ timerValue: this.timerValue, arraySize: this.arraySize });
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.worker) {
+      this.worker.terminate();
+    }
+  }
+
+  updateWorkerConfig(timerValue: number, arraySize: number) {
+    this.timerValue = timerValue;
+    this.arraySize = arraySize;
+    this.worker.postMessage({ timerValue, arraySize });
+  }
+
+  updateIds(additionalIds: string) {
+    this.additionalIds = additionalIds;
+    // logic for overwriting the ids of the data
+  }
 }
